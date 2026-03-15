b9b1ca10dc9ab78ab2858a571ef1aa8dbf792d64
Neto <netomarim@gmail.com>
2026-03-15 17:17:03 -0300

    Att host=0 e port=5000

--- reconhecimento.py
+++ reconhecimento.py
@@ -277,4 +277,4 @@ def reconhecer():
 # INICIAR
 # =========================
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=5000, debug=True, use_reloader=False)
